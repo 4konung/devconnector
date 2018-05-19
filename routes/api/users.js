49f88ee621e1bb6af24d0671c9d437cbe5f20d24
Vadim Konung <4konung@gmail.com>
2018-05-19 21:01:41 +0300

    ended with validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require("../../models/User");
@@ -31,7 +32,8 @@ router.post("/register", (req, res) => {
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exist" });
+      errors.email = "Email already exist";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200",
@@ -64,13 +66,20 @@ router.post("/register", (req, res) => {
 //@desc Login User / Return JWT Token
 //@access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find User
   User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -95,7 +104,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(404).json({ password: "Password incorrect" });
+        errors.password = "Password incorrect";
+        return res.status(404).json(errors);
       }
     });
   });
